a3d20dec3a797f55f13884462be467b924ac7cce
zhoumiaode <1010467470@qq.com>
2019-09-16 10:11:18 +0800

    first commit

---
+++ server-hi/src/main/java/com/forezp/serverhi/ServerHiApplication.java
@@ -0,0 +1,13 @@
+package com.forezp.serverhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerHiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerHiApplication.class, args);
+	}
+
+}
---
+++ server-hi/src/test/java/com/forezp/serverhi/ServerHiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.forezp.serverhi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServerHiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
